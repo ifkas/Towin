eebf2fa16840f33005039fdef7074a2f09d5a287
ifkas <themetracktor@gmail.com>
2024-11-18 10:56:52 +0100

    feat: Implemented Async Storage for shopping list persistence

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,65 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, View, TextInput, Text, ScrollView, FlatList } from "react-native";
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { Link } from "expo-router";
+import { getFromStorage, saveToStorage } from "../utils/storage";
+
+const storageKey = "shopping-list";
 
 type ShoppingListItem = {
   id: string;
   name: string;
   completedAtTimestamp?: number;
-  lastUpdatedTimestamp?: number;
+  lastUpdatedTimestamp: number;
 };
 
-const initialItems: ShoppingListItem[] = [
-  { id: "1", name: "Coffee" },
-  { id: "2", name: "Tea" },
-  { id: "3", name: "Sprite" },
-];
-
-import { useState } from "react";
+// const initialItems: ShoppingListItem[] = [
+//   { id: "1", name: "Coffee" },
+//   { id: "2", name: "Tea" },
+//   { id: "3", name: "Sprite" },
+// ];
 
 export default function App() {
-  const [items, setItems] = useState<ShoppingListItem[]>(initialItems);
+  // const [items, setItems] = useState<ShoppingListItem[]>(initialItems);
+  const [items, setItems] = useState<ShoppingListItem[]>([]);
   const [value, setValue] = useState<string>("");
+
+  useEffect(() => {
+    (async () => {
+      const data = await getFromStorage(storageKey);
+      if (data) {
+        setItems(data);
+      }
+    })();
+  }, []);
 
   const handleSubmit = () => {
     if (value.trim() === "") return;
     setItems([{ id: Math.random().toString(), name: value, lastUpdatedTimestamp: Date.now() }, ...items]);
+    saveToStorage(storageKey, [{ id: Math.random().toString(), name: value, lastUpdatedTimestamp: Date.now() }, ...items]);
     setValue("");
   };
 
   const handleDelete = (id: string) => {
     setItems(items.filter((item) => item.id !== id));
+    saveToStorage(
+      storageKey,
+      items.filter((item) => item.id !== id)
+    );
   };
 
   const handleComplete = (id: string) => {
     setItems(
+      items.map((item) => {
+        if (item.id === id) {
+          return { ...item, lastUpdatedTimestamp: Date.now(), completedAtTimestamp: item.completedAtTimestamp ? undefined : Date.now() };
+        }
+        return item;
+      })
+    );
+    saveToStorage(
+      storageKey,
       items.map((item) => {
         if (item.id === id) {
           return { ...item, lastUpdatedTimestamp: Date.now(), completedAtTimestamp: item.completedAtTimestamp ? undefined : Date.now() };
