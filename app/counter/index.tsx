5b553e5a3dad4029c6db3d78f7c6bbbdc488c24c
ifkas <themetracktor@gmail.com>
2024-11-25 10:19:06 +0100

    feat: Add countdown functionality and TimeSegment component for improved time display (date-fns)

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,35 @@
 // import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Touchable, TouchableOpacity, Alert } from "react-native";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import * as Notifications from "expo-notifications";
+import { Duration, isBefore, intervalToDuration } from "date-fns";
+
+// 10 seconds from now
+const timestamp = Date.now() + 10 * 1000;
+
+type CountdownStatus = {
+  isOverdue: boolean;
+  distance: Duration;
+};
 
 export default function CounterScreen() {
+  // const [secondsElapsed, setSecondsElapsed] = useState(0);
+  const [status, setStatus] = useState<CountdownStatus | null>(null);
+
+  // console.log(status);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const isOverdue = isBefore(timestamp, Date.now());
+      const distance = intervalToDuration(isOverdue ? { start: timestamp, end: Date.now() } : { start: Date.now(), end: timestamp });
+      setStatus({ isOverdue, distance });
+      // setSecondsElapsed((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   // const router = useRouter();
 
   // const handleRequestPermission = async () => {
@@ -34,6 +60,7 @@ export default function CounterScreen() {
         <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>Go to Idea</Text>
       </TouchableOpacity> */}
       {/* <Text style={styles.text}>This is the counter</Text> */}
+      {/* <Text>{secondsElapsed}</Text> */}
       <TouchableOpacity style={styles.button} activeOpacity={0.8} onPress={schedulePushNotification}>
         {/* <Text style={styles.text}>Request Permission</Text> */}
         <Text style={styles.text}>Schedule Notification</Text>
